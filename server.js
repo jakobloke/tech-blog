eeb2189d9e4561fb47c826bd07f0f0b51e84993b
Jakob <61665831+jakobloke@users.noreply.github.com>
2020-06-14 22:20:51 -0700

    tweaks

--- server.js
+++ server.js
@@ -3,15 +3,10 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
-app.use(session(sess));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
 const addDate = require('./utils/date');
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({addDate});
+const hbs = exphbs.create({ addDate });
 
 const session = require('express-session');
 const app = express();
@@ -21,13 +16,18 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret secret',
-    cookie: {maxAge: 36000},
+    cookie: { maxAge: 36000 },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
         db: sequelize
     })
 };
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
